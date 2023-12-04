483ff24bf16426cc68302b6d38c7bc6e4cfb9fc4
RubenPX <rubenmarchante@gmail.com>
2023-12-04 07:53:30 +0100

    rethink

--- app/src/shared/Client/ClientRouteDefinition.ts
+++
@@ -1,6 +0,0 @@
-export class ClientRouteDefinition<returnType, paramsType> {
-	constructor(
-		public readonly context: string,
-		public readonly method: string
-	) {}
-}
--- app/src/shared/EventMessage.ts
+++ app/src/shared/EventMessage.ts
@@ -1,15 +1,19 @@
-import { v4 } from 'uuid';
-
-export class EventMessage<outData = any, inData = any> {
-	public id: string = v4();
-
-	public returnData?: outData;
-	public requireObserver: boolean = false;
-	public requireReturn: boolean = false;
+export class EventMessage<out, eparams> {
+	public returnData: out | undefined = undefined;
+	public error: boolean = false;
 
 	constructor(
-		public readonly context: string,
-		public readonly method: string,
-		public readonly data?: inData
+		public context: string,
+		public method: string,
+		public params?: eparams
 	) {}
+
+	public static parseMessageEvent<out, prms>(ev: MessageEvent<any>): EventMessage<out, prms> {
+		const { context, method, params, ...rest } = ev.data;
+		const msgEvent = new EventMessage(context, method, params);
+
+		if (rest.error) msgEvent.error = rest.error;
+
+		return msgEvent as EventMessage<out, prms>;
+	}
 }
